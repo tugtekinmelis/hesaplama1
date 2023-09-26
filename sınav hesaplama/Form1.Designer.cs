67976c8315de60da3593dbfdc1f133b70ffdd2cf
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2023-09-26 15:09:20 +0300

    Add files via upload

---
+++ sınav hesaplama/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace sınav_hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNot1 = new System.Windows.Forms.Label();
+            this.lblNot2 = new System.Windows.Forms.Label();
+            this.lblsonuc = new System.Windows.Forms.Label();
+            this.txtNot1 = new System.Windows.Forms.TextBox();
+            this.txtNot2 = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNot1
+            // 
+            this.lblNot1.AutoSize = true;
+            this.lblNot1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNot1.Location = new System.Drawing.Point(57, 44);
+            this.lblNot1.Name = "lblNot1";
+            this.lblNot1.Size = new System.Drawing.Size(36, 17);
+            this.lblNot1.TabIndex = 1;
+            this.lblNot1.Text = "not1";
+            // 
+            // lblNot2
+            // 
+            this.lblNot2.AutoSize = true;
+            this.lblNot2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNot2.Location = new System.Drawing.Point(57, 81);
+            this.lblNot2.Name = "lblNot2";
+            this.lblNot2.Size = new System.Drawing.Size(36, 17);
+            this.lblNot2.TabIndex = 2;
+            this.lblNot2.Text = "not2";
+            // 
+            // lblsonuc
+            // 
+            this.lblsonuc.AutoSize = true;
+            this.lblsonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsonuc.Location = new System.Drawing.Point(57, 123);
+            this.lblsonuc.Name = "lblsonuc";
+            this.lblsonuc.Size = new System.Drawing.Size(46, 17);
+            this.lblsonuc.TabIndex = 3;
+            this.lblsonuc.Text = "sonuc";
+            // 
+            // txtNot1
+            // 
+            this.txtNot1.Location = new System.Drawing.Point(123, 44);
+            this.txtNot1.Multiline = true;
+            this.txtNot1.Name = "txtNot1";
+            this.txtNot1.Size = new System.Drawing.Size(100, 20);
+            this.txtNot1.TabIndex = 4;
+            // 
+            // txtNot2
+            // 
+            this.txtNot2.Location = new System.Drawing.Point(123, 81);
+            this.txtNot2.Multiline = true;
+            this.txtNot2.Name = "txtNot2";
+            this.txtNot2.Size = new System.Drawing.Size(100, 20);
+            this.txtNot2.TabIndex = 5;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(123, 168);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(100, 31);
+            this.btnHesapla.TabIndex = 7;
+            this.btnHesapla.Text = "hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtNot2);
+            this.Controls.Add(this.txtNot1);
+            this.Controls.Add(this.lblsonuc);
+            this.Controls.Add(this.lblNot2);
+            this.Controls.Add(this.lblNot1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNot1;
+        private System.Windows.Forms.Label lblNot2;
+        private System.Windows.Forms.Label lblsonuc;
+        private System.Windows.Forms.TextBox txtNot1;
+        private System.Windows.Forms.TextBox txtNot2;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ sınav hesaplama/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sınav_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int not1, not2;
+            double sonuc;
+
+            not1 = Convert.ToInt32(lblNot1.Text);
+            not2 = Convert.ToInt32(lblNot2.Text);
+
+            sonuc = (not1 + not2) / 2;
+
+            lblsonuc.Text = sonuc.ToString();
+        }
+    }
+}
